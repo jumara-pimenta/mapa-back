04a24aee1b62f822755d1d2a29f234a85b165e4d
Jumara Pimenta <jumara.pimenta@grupoicts.com.br>
2022-11-29 15:39:54 -0400

    fix: traducao das mensagens de erro em updateEmployee.dto

--- src/dtos/employee/updateEmployee.dto.ts
+++ src/dtos/employee/updateEmployee.dto.ts
@@ -9,43 +9,43 @@ import {
 } from 'class-validator';
 
 export class UpdateEmployeeDTO {
-  @IsString({ message: 'Registration não está definido como string.' })
+  @IsString({ message: 'Matrícula não está definida como string.' })
   @IsOptional()
   registration?: string;
 
-  @IsString({ message: 'Registration não está definido como string.' })
+  @IsString({ message: 'CPF não está definido como string.' })
   @IsOptional()
-  @MinLength(11, { message: 'CPF nao pode conter menos que 11 digitos.' })
-  @MaxLength(11, { message: 'CPF nao pode conter mais que 11 digitos.' })
+  @MinLength(11, { message: 'CPF não pode conter menos que 11 digitos.' })
+  @MaxLength(11, { message: 'CPF não pode conter mais que 11 digitos.' })
   cpf?: string;
 
-  @IsString({ message: 'Registration não está definido como string.' })
+  @IsString({ message: 'RG não está definido como string.' })
   @IsOptional()
-  @MinLength(9, { message: 'Rg nao pode conter menos que 9 digitos.' })
-  @MaxLength(11, { message: 'Rg nao pode conter mais que 9 digitos.' })
+  @MinLength(9, { message: 'Rg não pode conter menos que 9 digitos.' })
+  @MaxLength(11, { message: 'Rg não pode conter mais que 9 digitos.' })
   rg?: string;
 
   @IsOptional()
-  @IsDateString({ message: 'Registration não está definido como DateString.' })
+  @IsDateString({ message: 'Admissão não está definido como DateString.' })
   admission?: Date;
 
-  @IsString({ message: 'Registration não está definido como string.' })
+  @IsString({ message: 'Nome não está definido como string.' })
   @IsOptional()
   name?: string;
 
-  @IsString({ message: 'Registration não está definido como string.' })
+  @IsString({ message: 'Cargo não está definido como string.' })
   @IsOptional()
   role?: string;
 
-  @IsString({ message: 'Registration não está definido como string.' })
+  @IsString({ message: 'Turno não está definido como string.' })
   @IsOptional()
   shift?: string;
 
-  @IsString({ message: 'Registration não está definido como string.' })
+  @IsString({ message: 'Centro de custo não está definido como string.' })
   @IsOptional()
   costCenter?: string;
 
-  @IsString({ message: 'Registration não está definido como string.' })
+  @IsString({ message: 'Endereço não está definido como string.' })
   @IsOptional()
   address?: string;
 }
