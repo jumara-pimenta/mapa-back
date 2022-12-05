9ac9aa1ff0b7c93f61aef023de732f0f8dea6728
Jumara Pimenta <jumara.pimenta@grupoicts.com.br>
2022-12-05 14:50:14 -0400

    fix: validando campos para nao receber valor vazio

--- src/dtos/vehicle/updateVehicle.dto.ts
+++ src/dtos/vehicle/updateVehicle.dto.ts
@@ -1,3 +1,4 @@
+import { Transform, TransformFnParams } from 'class-transformer';
 import {
   IsBoolean,
   IsDate,
@@ -15,14 +16,17 @@ export class UpdateVehicleDTO {
   @IsOptional()
   @MinLength(7, { message: 'Placa não pode conter menos que 7 dígitos.' })
   @MaxLength(7, { message: 'Placa não pode conter mais que 7 dígitos.' })
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   plate?: string;
 
   @IsString({ message: 'Empresa não está definido para o tipo string.' })
   @IsOptional()
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   company?: string;
 
   @IsString({ message: 'Tipo não está definido para o tipo string.' })
   @IsOptional()
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   type?: string;
 
   @IsDateString()
@@ -39,6 +43,7 @@ export class UpdateVehicleDTO {
 
   @IsString({ message: 'Renavam não está definido para o tipo string.' })
   @IsOptional()
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   @MinLength(11, { message: 'Renavam não pode conter menos que 11 dígitos.' })
   @MaxLength(11, { message: 'Renavam nao pode conter mais que 11 dígitos.' })
   renavam?: string;
@@ -49,6 +54,7 @@ export class UpdateVehicleDTO {
 
   @IsString({ message: 'Observação não está definido para o tipo string.' })
   @IsOptional()
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   note?: string;
 
   @IsBoolean()
