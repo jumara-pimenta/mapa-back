f7cc110e4ecb2531c7ad839b8e432d96673f4d94
Jumara Pimenta <jumara.pimenta@grupoicts.com.br>
2022-12-05 14:59:51 -0400

    fix: validando campos para nao receber valor vazio

--- src/dtos/employee/createEmployee.dto.ts
+++ src/dtos/employee/createEmployee.dto.ts
@@ -1,3 +1,4 @@
+import { Transform, TransformFnParams } from 'class-transformer';
 import {
   IsString,
   IsNotEmpty,
@@ -10,18 +11,21 @@ import {
 export class CreateEmployeeDTO {
   @IsString({ message: 'Matrícula não está definida como string.' })
   @IsNotEmpty({ message: 'Matrícula não pode receber valor vazio.' })
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   registration: string;
 
   @IsString({ message: 'CPF não está definido como string.' })
   @IsNotEmpty({ message: 'CPF não pode receber valor vazio.' })
   @MinLength(11, { message: 'CPF não pode conter menos que 11 dígitos.' })
   @MaxLength(11, { message: 'CPF não pode conter mais que 11 dígitos.' })
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   cpf: string;
 
   @IsString({ message: 'RG não está definido como string.' })
   @IsNotEmpty({ message: 'RG não pode receber valor vazio.' })
   @MinLength(8, { message: 'RG nao pode conter menos que 8 dígitos.' })
   @MaxLength(11, { message: 'RG nao pode conter mais que 11 dígitos.' })
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   rg: string;
 
   @IsString({ message: 'Admissão não está definido como string.' })
@@ -31,21 +35,26 @@ export class CreateEmployeeDTO {
 
   @IsString({ message: 'Cargo não está definido como string.' })
   @IsNotEmpty({ message: 'Cargo não pode receber valor vazio.' })
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   role: string;
 
   @IsString({ message: 'Nome não está definido como string.' })
   @IsNotEmpty({ message: 'Nome não pode receber valor vazio.' })
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   name: string;
 
   @IsString({ message: 'Turno não está definido como string.' })
   @IsNotEmpty({ message: 'Turno não pode receber valor vazio.' })
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   shift: string;
 
   @IsString({ message: 'Centro de custo não está definido como string.' })
   @IsNotEmpty({ message: 'Centro de custo não pode receber valor vazio.' })
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   costCenter: string;
 
   @IsString({ message: 'Endereço não está definido como string.' })
   @IsNotEmpty({ message: 'Endereço não pode receber valor vazio.' })
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   address: string;
 }
--- src/dtos/employee/updateEmployee.dto.ts
+++ src/dtos/employee/updateEmployee.dto.ts
@@ -1,3 +1,4 @@
+import { Transform, TransformFnParams } from 'class-transformer';
 import {
   IsDate,
   IsDateString,
@@ -11,16 +12,19 @@ import {
 export class UpdateEmployeeDTO {
   @IsString({ message: 'Matrícula não está definida como string.' })
   @IsOptional()
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   registration?: string;
 
   @IsString({ message: 'CPF não está definido como string.' })
   @IsOptional()
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   @MinLength(11, { message: 'CPF não pode conter menos que 11 digitos.' })
   @MaxLength(11, { message: 'CPF não pode conter mais que 11 digitos.' })
   cpf?: string;
 
   @IsString({ message: 'RG não está definido como string.' })
   @IsOptional()
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   @MinLength(8, { message: 'Rg não pode conter menos que 8 digitos.' })
   @MaxLength(11, { message: 'Rg não pode conter mais que 11 digitos.' })
   rg?: string;
@@ -31,21 +35,26 @@ export class UpdateEmployeeDTO {
 
   @IsString({ message: 'Nome não está definido como string.' })
   @IsOptional()
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   name?: string;
 
   @IsString({ message: 'Cargo não está definido como string.' })
   @IsOptional()
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   role?: string;
 
   @IsString({ message: 'Turno não está definido como string.' })
   @IsOptional()
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   shift?: string;
 
   @IsString({ message: 'Centro de custo não está definido como string.' })
   @IsOptional()
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   costCenter?: string;
 
   @IsString({ message: 'Endereço não está definido como string.' })
   @IsOptional()
+  @Transform(({ value }: TransformFnParams) => value?.trim())
   address?: string;
 }
