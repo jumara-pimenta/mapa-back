6e34bd5dd7cfbf5a5eea77799fbb4e02cf153a0b
ricksonthompson <rickson.silva@grupoicts.com.br>
2022-12-29 13:55:02 -0400

    chore: translate message's dto from vehicles

--- src/dtos/vehicle/updateVehicle.dto.ts
+++ src/dtos/vehicle/updateVehicle.dto.ts
@@ -1,60 +1,73 @@
 import { Transform, TransformFnParams } from 'class-transformer';
 import {
   IsBoolean,
-  IsDate,
   IsDateString,
-  IsEnum,
   IsNumber,
   IsOptional,
   IsString,
+  Length,
   MaxLength,
   MinLength,
 } from 'class-validator';
 
 export class UpdateVehicleDTO {
-  @IsString({ message: 'Placa não está definido para o tipo string.' })
-  @IsOptional()
-  @MinLength(7, { message: 'Placa não pode conter menos que 7 dígitos.' })
-  @MaxLength(7, { message: 'Placa não pode conter mais que 7 dígitos.' })
+  @IsString({ message: '[plate] A placa deve ser do tipo string.' })
+  @Length(7, 7, { message: '[plate] A placa deve possuir 7 caracteres.' })
   @Transform(({ value }: TransformFnParams) => value?.trim())
+  @IsOptional()
   plate?: string;
 
-  @IsString({ message: 'Empresa não está definido para o tipo string.' })
+  @IsString({ message: '[company] O nome da empresa deve ser do tipo string.' })
   @IsOptional()
   @Transform(({ value }: TransformFnParams) => value?.trim())
   company?: string;
 
-  @IsString({ message: 'Tipo não está definido para o tipo string.' })
+  @IsString({ message: '[type] O tipo deve ser do tipo string.' })
   @IsOptional()
   @Transform(({ value }: TransformFnParams) => value?.trim())
   type?: string;
 
-  @IsDateString()
+  @IsDateString(
+    {},
+    { message: '[lastSurvey] A última vistoria deve ser do tipo date.' },
+  )
   @IsOptional()
   lastSurvey?: Date;
 
-  @IsDateString()
+  @IsDateString(
+    {},
+    { message: '[expiration] A expiração deve ser do tipo date.' },
+  )
   @IsOptional()
   expiration?: Date;
 
-  @IsNumber()
+  @IsNumber(
+    { allowInfinity: true },
+    { message: '[capacity] A capacidade deve ser do tipo number.' },
+  )
   @IsOptional()
   capacity?: number;
 
-  @IsString({ message: 'Renavam não está definido para o tipo string.' })
-  @IsOptional()
+  @IsString({ message: '[renavam] O RENAVAM deve ser do tipo string.' })
+  @Length(11, 11, {
+    message: '[renavam] O RENAVAM deve possuir 11 caracteres.',
+  })
   @Transform(({ value }: TransformFnParams) => value?.trim())
-  @MinLength(11, { message: 'Renavam não pode conter menos que 11 dígitos.' })
-  @MaxLength(11, { message: 'Renavam nao pode conter mais que 11 dígitos.' })
+  @IsOptional()
   renavam?: string;
 
-  @IsDateString()
+  @IsDateString(
+    {},
+    { message: '[lastMaintenance] A última manutenção deve ser do tipo date.' },
+  )
   @IsOptional()
   lastMaintenance?: Date;
 
-  @IsString({ message: 'Observação não está definido para o tipo string.' })
-  @IsOptional()
+  @IsString({
+    message: '[note] O campo de observação deve ser do tipo string.',
+  })
   @Transform(({ value }: TransformFnParams) => value?.trim())
+  @IsOptional()
   note?: string;
 
   @IsBoolean({
