3fb4ea612f112168ffedc5e01e4729ddb4968a31
ricksonthompson <rickson.silva@grupoicts.com.br>
2022-12-29 13:54:18 -0400

    chore: translate message's dto from driver

--- src/dtos/driver/createDriver.dto.ts
+++ src/dtos/driver/createDriver.dto.ts
@@ -2,38 +2,38 @@ import { Transform, TransformFnParams } from 'class-transformer';
 import { IsString, IsNotEmpty, IsDateString, Length } from 'class-validator';
 
 export class CreateDriverDTO {
-  @IsString({ message: '[name] O campo NAME deve ser do tipo string.' })
-  @IsNotEmpty({ message: '[name] O campo NAME deve ser preenchido.' })
+  @IsString({ message: '[name] O nome deve ser do tipo string.' })
+  @IsNotEmpty({ message: '[name] O nome deve ser preenchido.' })
   @Length(10, 255, {
-    message: '[name] O campo NAME deve conter no mínimo 10 caracteres.',
+    message: '[name] O nome deve possuir no mínimo 10 caracteres.',
   })
   @Transform(({ value }: TransformFnParams) => value?.trim())
   name: string;
 
-  @IsString({ message: '[cpf] O campo CPF deve ser do tipo string.' })
-  @IsNotEmpty({ message: '[cpf] O campo CPF deve ser preenchido.' })
-  @Length(11, 11, { message: '[cpf] O campo CPF deve conter 11 caracteres.' })
+  @IsString({ message: '[cpf] O CPF deve ser do tipo string.' })
+  @IsNotEmpty({ message: '[cpf] O CPF deve ser preenchido.' })
+  @Length(11, 11, { message: '[cpf] O CPF deve conter 11 caracteres.' })
   @Transform(({ value }: TransformFnParams) => value?.trim())
   cpf: string;
 
-  @IsString({ message: '[cnh] O campo CNH deve ser do tipo string.' })
-  @IsNotEmpty({ message: '[cnh] O campo CNH deve ser preenchido.' })
+  @IsString({ message: '[cnh] A CNH deve ser do tipo string.' })
+  @IsNotEmpty({ message: '[cnh] A CNH deve ser preenchida.' })
   @Transform(({ value }: TransformFnParams) => value?.trim())
   cnh: string;
 
   @IsDateString(
     {},
     {
-      message: '[validation] O campo VALIDATION deve ser do tipo Date valido.',
+      message: '[validation] A validade deve ser do tipo date.',
     },
   )
   @IsNotEmpty({
-    message: '[validation] O campo VALIDATION deve ser preenchido.',
+    message: '[validation] A validade deve ser preenchida.',
   })
   validation: Date;
 
-  @IsString({ message: '[category] O campo CATEGORY deve ser do tipo string.' })
-  @IsNotEmpty({ message: '[category] O campo CATEGORY deve ser preenchido.' })
+  @IsString({ message: '[category] A categoria deve ser do tipo string.' })
+  @IsNotEmpty({ message: '[category] A categoria deve ser preenchida.' })
   @Transform(({ value }: TransformFnParams) => value?.trim())
   @Length(1, 2)
   category: string;
