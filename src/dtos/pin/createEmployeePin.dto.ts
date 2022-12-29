87a036ecbe722131a19bb2dff5c886c7a5ecb615
ricksonthompson <rickson.silva@grupoicts.com.br>
2022-12-29 13:55:31 -0400

    chore: translate message's dto from pins

--- src/dtos/pin/createEmployeePin.dto.ts
+++ src/dtos/pin/createEmployeePin.dto.ts
@@ -2,36 +2,38 @@ import {
   IsString,
   IsEnum,
   IsOptional,
+  IsNotEmpty,
 } from 'class-validator';
 import { ECreatePin } from '../../utils/ETypes';
 
 export class CreateEmployeePinDTO {
   @IsEnum(ECreatePin, {
-    message: '[typeCreation] não está definido como enum.',
+    message: '[typeCreation] O tipo de criação deve ser do tipo enum: EXISTENTE | NOVO',
   })
+  @IsNotEmpty({ message: '[typeCreation] O tipo de criação deve ser preenchido.'})
   typeCreation: ECreatePin;
 
-  @IsString({ message: '[id] não está definido como string.' })
+  @IsString({ message: '[id] O id do ponto de embarque deve ser do tipo string.' })
   @IsOptional()
   id?: string;
 
-  @IsString({ message: '[lat] não está definido como string.' })
+  @IsString({ message: '[lat] A latitude deve ser do tipo string.' })
   @IsOptional()
   lat?: string;
 
-  @IsString({ message: '[lng] não está definido como string.' })
+  @IsString({ message: '[lng] A longitude deve ser do tipo string.' })
   @IsOptional()
   lng?: string;
 
-  @IsString({ message: '[local] não está definido como string.' })
+  @IsString({ message: '[local] O local deve ser do tipo string.' })
   @IsOptional()
   local?: string;
 
-  @IsString({ message: '[details] não está definido como string.' })
+  @IsString({ message: '[details] O campo de detalhes deve ser do tipo string.' })
   @IsOptional()
   details?: string;
 
-  @IsString({ message: '[title] não está definido como string.' })
+  @IsString({ message: '[title] O título deve ser do tipo string.' })
   @IsOptional()
   title?: string;
 }
--- src/dtos/pin/createPin.dto.ts
+++ src/dtos/pin/createPin.dto.ts
@@ -2,27 +2,27 @@ import { Transform, TransformFnParams } from 'class-transformer';
 import { IsString, IsNotEmpty, IsLongitude, IsLatitude } from 'class-validator';
 
 export class CreatePinDTO {
-  @IsString({ message: 'Título tem que ser do tipo string' })
-  @IsNotEmpty({ message: 'O campo Título não pode ser vazio' })
+  @IsString({ message: '[title] O título deve ser do tipo string.' })
+  @IsNotEmpty({ message: '[title] O título deve ser preenchido.' })
   @Transform(({ value }: TransformFnParams) => value?.trim())
   title: string;
 
-  @IsString({ message: 'Local tem que ser do tipo string' })
-  @IsNotEmpty({ message: 'O campo Local não pode ser vazio' })
+  @IsString({ message: '[local] O local deve ser do tipo string.' })
+  @IsNotEmpty({ message: '[local] O local deve ser preenchido.' })
   @Transform(({ value }: TransformFnParams) => value?.trim())
   local: string;
 
-  @IsString({ message: 'Detalhes tem que ser do tipo string' })
-  @IsNotEmpty({ message: 'O campo Detalhes não pode ser vazio' })
+  @IsString({ message: '[details] O campo de detalhes deve ser do tipo string.' })
+  @IsNotEmpty({ message: '[details] O campo de detalhes deve ser preenchido.' })
   @Transform(({ value }: TransformFnParams) => value?.trim())
   details: string;
 
-  @IsLatitude({ message: '[lat] A latitude deve ser do formato correto.'})
+  @IsLatitude({ message: '[lat] A latitude deve ser do tipo string.'})
   @IsNotEmpty({ message: '[lat] A latitude deve ser preenchida.' })
   @Transform(({ value }: TransformFnParams) => value?.trim())
   lat: string;
 
-  @IsLongitude({ message: '[lng] A longitude deve ser do formato correto.'})
+  @IsLongitude({ message: '[lng] A longitude deve ser do tipo string.'})
   @IsNotEmpty({ message: '[lng] A longitude deve ser preenchida.' })
   @Transform(({ value }: TransformFnParams) => value?.trim())
   lng: string;
