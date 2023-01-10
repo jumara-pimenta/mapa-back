6fa9ddd93afbd0f661b05afba440b8078ed58771
Marcus Vinícius Peixoto Paulo <marcus.paulo@grupoicts.com.br>
2023-01-10 17:43:29 -0400

    feat: init auth implementation

---
+++ src/entities/backOfficeUser.entity.ts
@@ -0,0 +1,21 @@
+import { getDateInLocaleTime } from '../utils/Date';
+import { v4 as uuid } from 'uuid';
+
+export class BackOfficeUser {
+  id: string;
+  email: string;
+  password: string;
+  name: string;
+  role: string;
+  createdAt: Date;
+  updatedAt?: Date;
+
+  constructor(
+    props: Omit<BackOfficeUser, 'id' | 'createdAt' | 'route'>,
+    id?: string,
+  ) {
+    Object.assign(this, props);
+    this.createdAt = getDateInLocaleTime(new Date());
+    this.id = id ?? uuid();
+  }
+}
