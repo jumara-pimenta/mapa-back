ad3a686e240580471ca2a48943fcf9221b06084f
Jumara Pimenta <jumara.pimenta@grupoicts.com.br>
2023-01-19 15:02:50 -0400

    fix: example swagger and module

--- src/modules/routeHistory.module.ts
+++ src/modules/routeHistory.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { RouteHistoryService } from '../services/routeHistory.service';
 import { RouteHistoryController } from '../controllers/routeHistory.controller';
 import { RouteHistoryRepository } from '../repositories/routeHistory/routeHistory.repository';
-import { RouteModule } from './route.module';
+import { PathModule } from './path.module';
+import { DriverModule } from './driver.module';
+import { VehicleModule } from './vehicle.module';
 
 @Module({
-  imports: [RouteModule],
+  imports: [PathModule, DriverModule, VehicleModule],
   controllers: [RouteHistoryController],
   providers: [
     RouteHistoryService,
