a50d518b1a4b049f002e1a394f276856712f357b
PedroSoprano <pedro.soprano@grupoicts.com.br>
2023-05-19 12:22:52 -0400

    feature: criação de rotas e serviços para primeiro acesso dos colabroadores e motoristas

--- src/dtos/driver/firstAccess.dto.ts
+++ src/dtos/driver/firstAccess.dto.ts
@@ -19,12 +19,13 @@ export class FirstAccessDriverDTO {
   @IsString({ message: '[password] O campo password deve ser alfanumérico.'})
   @IsNotEmpty({ message: '[password] O campo password deve ser preenchido.' })
   @Length(7, 100, { message: '[password] O campo password deve conter mais de 7 caracteres.' })
-  @Matches(passwordRgx)
+  @Matches(passwordRgx, {message: '[password] Este campo deve conter pelo menos um caractere especial e um caractere maiúsculo.'})
   @Transform(({ value }: TransformFnParams) => value?.trim())
   password: string
 
   @IsString({ message: '[confirmPassword] O campo confirmPassword deve ser alfanumérico.'})
   @IsNotEmpty({ message: '[confirmPassword] O campo confirmPassword deve ser preenchido.' })
+  @Matches(passwordRgx, {message: '[confirmPassword] Este campo deve conter pelo menos um caractere especial e um caractere maiúsculo.'})
   @Transform(({ value }: TransformFnParams) => value?.trim())
   confirmPassword: string
 
