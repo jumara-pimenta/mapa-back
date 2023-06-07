878551514aed012c628d13074ce0e1cce0abb9c5
ricksonthompson <rickson.silva@grupoicts.com.br>
2023-06-07 17:51:44 -0400

    chore: indent spaces

--- src/services/path.service.ts
+++ src/services/path.service.ts
@@ -246,7 +246,9 @@ export class PathService {
 
   async generate(props: CreatePathDTO): Promise<void> {
     const { type, duration, startsAt, startsReturnAt } = props.details;
+    
     const route = await this.routeService.listById(props.routeId);
+
     if (type === ETypePath.ONE_WAY || type === ETypePath.RETURN) {
       const path = await this.pathRepository.create(
         new Path(
@@ -278,6 +280,7 @@ export class PathService {
           route,
         ),
       );
+
       const pathReturn = await this.pathRepository.create(
         new Path(
           {
