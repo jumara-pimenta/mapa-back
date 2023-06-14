f1f2baced1456aa738207c9949b1483ef2a3c507
Rickson Thompson <rickson.silva@grupoicts.com.br>
2023-06-14 12:39:39 -0400

    chore: implemented circulaty dependences on pin module

--- src/modules/pin.module.ts
+++ src/modules/pin.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { PinController } from '../controllers/pin.controller';
 import { PinRepository } from '../repositories/pin/pin.repository';
 import { PinService } from '../services/pin.service';
+import { EmployeesOnPinModule } from './employeesOnPin.module';
 
 @Module({
+  imports: [forwardRef(() => EmployeesOnPinModule)],
   controllers: [PinController],
   providers: [
     PinService,
